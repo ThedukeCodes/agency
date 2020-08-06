3b00f8a3f890694e227811c71ad0cdc5b94b2c05
Diego Riera <rieradid@tcd.ie>
2020-08-06 10:46:43 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 export AUTH0_DOMAIN='dev-2-s-zb2j.eu.auth0.com'
 export API_AUDIENCE='capstone'
 export ALGORITHMS=['RS256']
-export DATABASE_URL='postgresql://postgres:Ioneryle1@localhost:5432/capstone'
+export DATABASE_URL='postgresql://postgres:*******@localhost:5432/capstone'
